9d73af8607b67beaf62ccd8b5458ad384d28faad
iskandarherputra <iskandarherputra@gmail.com>
2020-06-30 17:54:19 +0700

    modified storyboard and view controller

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -9,12 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var questionNumber = 1
+    var tempCorrectAnswer = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Score", style: .plain, target: self, action: #selector(scoreTapped))
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0).cgColor
+        button2.layer.borderColor = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0).cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion(action: nil)
     }
-
-
+    
+    func askQuestion(action: UIAlertAction!) {
+        countries.shuffle()
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        if questionNumber <= 10 {
+            title = "\(questionNumber). \(countries[correctAnswer].uppercased()) - Score : \(score)"
+        } else {
+            title = "Game over - Score : \(score)"
+        }
+        
+        
+        
+        if questionNumber > 10 {
+            let ac = UIAlertController(title: "Game over", message: "Final score : \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Play again", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+            
+            score = 0
+            questionNumber = 1
+            tempCorrectAnswer = correctAnswer
+        }
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+            questionNumber += 1
+        } else {
+            title = "Wrong! that's the flag of \(countries[sender.tag])"
+            score -= 1
+            questionNumber += 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
+    @objc func scoreTapped() {
+        let ac = UIAlertController(title: "Your current score", message: "\(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
+    
 }
 
